29460263635131034093ddbe66d2b56114d6236d
harshilpec17 <harshilsuthar1995@gmail.com>
2023-11-01 17:06:36 -0400

    filtered data added for filter button

--- src/utils/redux/GPTSearchSlice.js
+++ src/utils/redux/GPTSearchSlice.js
@@ -4,14 +4,31 @@ const GPTSearchSlice = createSlice({
   name: "gpt",
   initialState: {
     gptToggleValue: false,
+    gptSearchResults: null,
+    gptMovieDataBaseResults: null,
+    filteredGptMovieDataBaseResult: null,
   },
   reducers: {
     GptToggle: (state) => {
       state.gptToggleValue = !state.gptToggleValue;
     },
+    addGptSearchResult: (state, action) => {
+      state.gptSearchResults = action.payload;
+    },
+    addGptMovieDataBaseResults: (state, action) => {
+      state.gptMovieDataBaseResults = action.payload;
+    },
+    addFilteredGptMovieDataBaseResults: (state, action) => {
+      state.filteredGptMovieDataBaseResult = action.payload;
+    },
   },
 });
 
-export const { GptToggle } = GPTSearchSlice.actions;
+export const {
+  GptToggle,
+  addGptSearchResult,
+  addGptMovieDataBaseResults,
+  addFilteredGptMovieDataBaseResults,
+} = GPTSearchSlice.actions;
 
 export default GPTSearchSlice.reducer;
