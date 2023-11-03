da34b376b752eaeb8690f3c6b9f1b945cd1ab19e
harshilpec17 <harshilsuthar1995@gmail.com>
2023-11-03 14:17:03 -0400

    secure the API key

--- src/utils/Constants/constants.js
+++ src/utils/Constants/constants.js
@@ -7,16 +7,20 @@ export const PROFILE_IMG =
 export const BACKGROUND_IMG =
   "https://assets.nflxext.com/ffe/siteui/vlv3/ab180a27-b661-44d7-a6d9-940cb32f2f4a/f9a24fdc-ab8b-4e19-b0f5-4923356a72de/CA-en-20231009-popsignuptwoweeks-perspective_alpha_website_medium.jpg";
 
+export const OPEN_AI_BACKGROUND_IMG =
+  "https://imageio.forbes.com/specials-images/imageserve/65133ef75b24f850bb4ad501/0x0.jpg?format=jpg&crop=3988,2244,x0,y198,safe&height=900&width=1600&fit=bounds";
+
 export const API_TOKEN = {
   method: "GET",
   headers: {
     accept: "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjOWM3NWZhZTNmMWRkOWE4NmUwYjQ2ZjRlNjM0MTQ2ZSIsInN1YiI6IjY1MzE0Mzk3ZjE3NTljMDBlMmYwMmVlYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.uVpl_jjCg75SPxU7tTE-J6TIoIC31XwXfFZW0Hzw9k8",
+    Authorization: process.env.REACT_APP_API_TOKEN,
   },
 };
 
 export const MOVIE_IMAGE_URL = "https://image.tmdb.org/t/p/w500";
+
+export const OPEN_AI = process.env.REACT_APP_OPENAI_API_KEY;
 
 export const langs = [
   {
