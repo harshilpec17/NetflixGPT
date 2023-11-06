c34e14c6d5d8b92a1ae7e38fd2be200300fe152c
harshilpec17 <harshilsuthar1995@gmail.com>
2023-11-06 14:44:02 -0500

    code clean up && store the URL in constant

--- src/component/GPTPage/GptPage.js
+++ src/component/GPTPage/GptPage.js
@@ -1,16 +1,17 @@
 import React from "react";
 import GptSearchBar from "./GPTPageComponents/GptSearchBar";
 import GptSearchResult from "./GPTPageComponents/GptSearchResult";
+import { OPEN_AI_BACKGROUND_IMG } from "../../utils/Constants/constants";
 
 const GptPage = () => {
   return (
     <>
       <img
-        className="bg-cover bg-center h-screen md:bg-contain absolute w-screen"
-        src="https://imageio.forbes.com/specials-images/imageserve/65133ef75b24f850bb4ad501/0x0.jpg?format=jpg&crop=3988,2244,x0,y198,safe&height=900&width=1600&fit=bounds"
+        className="bg-cover bg-center h-screen md:bg-contain fixed w-screen"
+        src={OPEN_AI_BACKGROUND_IMG}
         alt="background Img"
       />
-      <div className="flex flex-col">
+      <div className="flex flex-col bg-black">
         <GptSearchBar />
         <GptSearchResult />
       </div>
