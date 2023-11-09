509c267266e992fa68ee87125af24b843e4ba745
harshilpec17 <harshilsuthar1995@gmail.com>
2023-11-09 14:21:29 -0500

    Movie Detail popUp added

--- src/component/MovieClusters/MovieClusterComponents/MovieCards.js
+++ src/component/MovieClusters/MovieClusterComponents/MovieCards.js
@@ -1,15 +1,26 @@
 import React from "react";
 import { MOVIE_IMAGE_URL } from "../../../utils/Constants/constants";
+import Popup from "reactjs-popup";
+import MovieDetailsCard from "./MovieDetails";
 
-const MovieCards = ({ title, poster }) => {
+const MovieCards = ({ title, poster, movie }) => {
+  if (!poster) return null;
   return (
     <>
       <h1>{title}</h1>
-      <div>
-        <div className="w-48 pr-4">
-          <img alt="Movie Poster" src={MOVIE_IMAGE_URL + poster}></img>
-        </div>
-      </div>
+      <Popup
+        trigger={
+          <div>
+            <div className="w-48  my-4 mr-4 hover:shadow-[0px_0px_10px_2px_#660033] hover:scale-110 delay-150 duration-300 ease-in-out">
+              <img alt="Movie Poster" src={MOVIE_IMAGE_URL + poster}></img>
+            </div>
+          </div>
+        }
+        modal
+        nested
+      >
+        <MovieDetailsCard details={movie} />
+      </Popup>
     </>
   );
 };
