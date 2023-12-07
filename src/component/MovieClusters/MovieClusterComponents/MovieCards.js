aacae29f5d46d8cd8fb69fb21b5c60e52cf6a19d
harshilpec17 <harshilsuthar1995@gmail.com>
2023-12-06 22:32:44 -0500

    add: function to avoid bad state call

--- src/component/MovieClusters/MovieClusterComponents/MovieCards.js
+++ src/component/MovieClusters/MovieClusterComponents/MovieCards.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { MOVIE_IMAGE_URL } from "../../../utils/Constants/constants";
 import Popup from "reactjs-popup";
 import MovieDetailsCard from "./MovieDetailsCard";
@@ -7,17 +7,22 @@ import { addSelectedMovie } from "../../../utils/redux/movieDescription";
 
 const MovieCards = ({ title, poster, movie, id }) => {
   const dispatch = useDispatch();
+
+  const handleClick = () => {
+    dispatch(addSelectedMovie(movie));
+  };
+
   if (!poster) return null;
-
-  dispatch(addSelectedMovie(movie));
-
   return (
     <>
       <h1>{title}</h1>
       <Popup
         trigger={
           <div className="">
-            <div className="w-48 my-4 mx-2 hover:scale-110 delay-150 duration-300 ease-in-out">
+            <div
+              onClick={handleClick}
+              className="w-48 my-4 mx-2 shadow-2xl hover:scale-110 delay-150 duration-300 ease-in-out"
+            >
               <img alt="Movie Poster" src={MOVIE_IMAGE_URL + poster}></img>
             </div>
           </div>
