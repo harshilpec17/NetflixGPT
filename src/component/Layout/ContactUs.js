be5942583325da4088b25a16ab061a7f972edc09
harshilpec17 <harshilsuthar1995@gmail.com>
2024-02-20 22:47:38 -0500

    add: Icon to tab

--- src/component/Layout/ContactUs.js
+++ src/component/Layout/ContactUs.js
@@ -2,7 +2,6 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 const ContactUs = () => {
-  const user = useSelector((store) => store.user);
   return <div>ContactUs</div>;
 };
 
